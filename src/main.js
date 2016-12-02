d6c2daf852a368d60c0ef2c2b2baeae2376e8755
VidyullathaKandipati <latha522@gmail.com>
2016-12-02 18:01:13 +1100

    Chap 9 Assignment

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-var card1 = "king";
+var card1 = "queen";
 var card2 = "king";
 var card3 = "queen";
-var card4 = "queen";
+var card4 = "king";
+
+if (card2 === card4)
+{
+	alert("You found a match.");
+}
+else
+{
+	alert("Sorry, try again");
+}
